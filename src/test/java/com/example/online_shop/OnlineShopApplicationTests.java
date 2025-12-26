28ac618d5de8d1d32b0bc705ac9d7bbdf414085b
Maria Khlestova <selebina.maria@yandex.ru>
2025-12-26 19:00:08 +0300

    update

--- src/test/java/com/example/online_shop/OnlineShopApplicationTests.java
+++ src/test/java/com/example/online_shop/OnlineShopApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.online_shop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class OnlineShopApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    
+    @Test
+    void simpleTest() {
+        System.out.println("Тест работает");
+    }
 }
